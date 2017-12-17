d64609c91f06f348e12c57851845dbf689580abd
smokfyz <ivansharapenkov@gmail.com>
2017-12-17 14:09:25 +0300

    add mobile device checker

--- physics.js
+++ physics.js
@@ -21,6 +21,31 @@ if(!storage.getItem('score')) {
 
 land = new CreatePhysicalObj(land);
 
+var isMobile = {
+    Android: function() {
+        return navigator.userAgent.match(/Android/i);
+    },
+    BlackBerry: function() {
+        return navigator.userAgent.match(/BlackBerry/i);
+    },
+    iOS: function() {
+        return navigator.userAgent.match(/iPhone|iPad|iPod/i);
+    },
+    Opera: function() {
+        return navigator.userAgent.match(/Opera Mini/i);
+    },
+    Windows: function() {
+        return navigator.userAgent.match(/IEMobile/i);
+    },
+    any: function() {
+        return (isMobile.Android() || isMobile.BlackBerry() || isMobile.iOS() || isMobile.Opera() || isMobile.Windows());
+    }
+};
+
+if(isMobile.any()) {
+    $('body').css({'font-size': '32px'});
+}
+
 function CreatePhysicalObj(jqueryEl) {
     this.x = jqueryEl.position().left;
     this.y = jqueryEl.position().top;
